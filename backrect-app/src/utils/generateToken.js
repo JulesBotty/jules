e7bcc519b5b6c46667f9a7e4d1408899b2ddbe96
JulesBotty <julesbotty33@gmail.com>
2026-08-28 09:44:48 +0000

    sssfff

--- backrect-app/src/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (user) =>
-  jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "1d"
-  });
-
-export default generateToken;
